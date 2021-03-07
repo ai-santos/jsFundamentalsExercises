7449c69c2b4ded8342523152d16fe58d3ce0e0ca
Aileen Santos <asantos3026@gmail.com>
2021-03-06 21:18:30 -0800

    Codecademy practice

---
+++ team_stats.js
@@ -0,0 +1,20 @@
+const team = {
+  _players: [
+    {  
+        firstName: 'Pablo',
+        lastName: 'Sanchez',
+        age: 11
+    }, 
+    {
+        firstName: 'Tony',
+        lastName: 'Micelli',
+        age: 32
+    }, 
+    {
+        firstName: 'Reggie',
+        lastName: 'Jackson',
+        age: 31
+    }
+  ],
+  _games: []
+}
