69cf5350255c0e0975f70c88e03d6e149a2a2a2e
Aileen Santos <asantos3026@gmail.com>
2021-03-06 22:00:36 -0800

    Object with get methods

--- team_stats.js
+++ team_stats.js
@@ -16,5 +16,49 @@ const team = {
         age: 31
     }
   ],
-  _games: []
+  _games: [
+    {
+        opponent: 'Broncos',
+        teamPoints: 42,
+        opponentPoints: 27
+    },
+    {
+        opponent: 'Cardinals',
+        teamPoints: 14,
+        opponentPoints: 7
+    },
+    {
+        opponent: 'Eagles',
+        teamPoints: 24,
+        opponentPoints: 9
+    },
+  ],
+  get players() {
+    return this._players;
+  },
+  get games() {
+    return this._players;
+  },
+  addPlayer(firstName, lastName, age) {
+    const player = {
+        firstname: firstName,
+        lastName: lastName,
+        age: age 
+    };
+    return this._players.push(player);
+  },
+  addGame(opponentName, teamPoints, opponentPoints) {
+    const game = {
+        opponentName: opponentName,
+        teamPoints: teamPoints,
+        opponentPoints: opponentPoints
+    }
+    return this._games.push(game);
+  }
 }
+team.addPlayer('Steph', 'Curry', 28);
+team.addPlayer('Lisa', 'Leslie', 44);
+team.addPlayer('Bugs', 'Bunny', 76);
+console.log(team._players);
+team.addGame('Buccaneers', 29, 17);
+console.log(team._games);
