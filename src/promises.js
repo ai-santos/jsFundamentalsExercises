1dcba6994b98156d27b482454fa8e4b49b8800f6
Aileen Santos <asantos3026@gmail.com>
2021-03-10 21:24:40 -0800

    Promises practice

--- src/promises.js
+++ src/promises.js
@@ -4,6 +4,31 @@ const inventory = {
   bags: 1344
 };
 
+const order = [['sunglasses', 1], ['bags', 2]];
+
+const checkInventory = (order) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let inStock = order.every(item => inventory[item[0]] >= item[1]);
+      if (inStock) {
+        resolve(`Thank you. Your order was successful.`);
+      } else {
+        reject(`We're sorry. Your order could not be completed because some items are sold out.`);
+      }
+    }, 1000);
+  })
+};
+
+const handleSuccess = (resolve) => {
+  console.log(resolve);
+};
+
+const handleFailure = (reject) => {
+  console.log(reject);
+};
+
+checkInventory(order).then(handleSuccess, handleFailure);
+
 // Write your code below:
 const myExecutor = (resolve, reject) => {
   if(inventory.sunglasses > 0) {
